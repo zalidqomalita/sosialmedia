a140b0d52efc0acea339a3e07065f3aa78253b22
zalidqomalita <58623525+zalidqomalita@users.noreply.github.com>
2025-05-07 08:47:36 +0700

    Update run_gui_sosmed_analytics_v2.py

--- run_gui_sosmed_analytics_v2.py
+++ run_gui_sosmed_analytics_v2.py
@@ -209,8 +209,8 @@ class BertClassifier(nn.Module):
 def get_topik(kata):
     print("------------Load Model----------")
     checkpoint = 'indolem/indobertweet-base-uncased'
-    indobert = model(checkpoint, freeze=True)
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    indobert = AutoModel.from_pretrained(checkpoint, token=True)
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint, token=True)
 
     model_path = hf_hub_download(
         repo_id="zqomalita/kelas-topik",
